eef9be0577672c6d13d0c7399f901e5c800df9d7
Артём <omegascary@yandex.ru>
2025-10-19 02:51:36 +0300

    Выполнено упражнение 1.8 из книги Кернигана и Ритчи.

---
+++ week01-c-fundamentals/exercises/d2_c1_e1-8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    //Упражнение 1.8. Напишите программу для подсчета пробелов, табуляций и новых строк.
+
+    long blanks = 0, tabs = 0, newlines = 0, c;
+
+    while (1) {
+        c = getchar();
+
+        if (c == '\n') {
+            ++newlines;
+        } else if (c == '\t') {
+            ++tabs;
+        } else if (c == ' ') {
+            ++blanks;
+        }
+
+        if (c == EOF) {
+            break;
+        }
+    }
+
+    printf("Количество пробелов: %ld\n", blanks);
+    printf("Количество табуляций: %ld\n", tabs);
+    printf("Количество новых строк: %ld\n", newlines);
+
+    return 0;
+}
