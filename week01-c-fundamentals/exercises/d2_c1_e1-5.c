3f644702f0749baf5967d01454a4856eddb16dc7
Артём <omegascary@yandex.ru>
2025-10-19 02:26:37 +0300

    Выполнено упражнение 1.5 из книги Кернигана и Ритчи.

--- week01-c-fundamentals/exercises/d2_c1_e1-5.c
+++ week01-c-fundamentals/exercises/d2_c1_e1-5.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-/* печать таблицы температур по Фаренгейту и Цельсию для
-fahr = 0, 20 ... 300; вариант с плавающей точкой */
-
 int main() {
     float fahr, celsius;
     int lower, upper, step;
@@ -25,7 +22,7 @@ int main() {
         Изменить программу преобразования температур так, чтобы она печатала таблицу в
         обратном порядке, т. е. от 300 до 0.
     */
-    
+
     for(fahr = upper; fahr >= lower; fahr -= step) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf ("%3.0f %6.1f\n", fahr, celsius);
